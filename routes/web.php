1f54cc43a39578e7e5c7055aa682f43655dc6980
PuNKeR <maximkoritin@gmail.com>
2016-11-01 17:41:07 +0300

    add tempalte

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/', function () {
+        return view('index');
+    });
+
+});

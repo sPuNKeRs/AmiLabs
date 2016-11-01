e142d426d6ab839db2359fe8088c5124e8aa04b9
PuNKeR <maximkoritin@gmail.com>
2016-11-01 17:53:53 +0300

    some change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,4 +2,30 @@
 
 @section('content')
 
+<div class="top-right links">
+                    @if (Auth::check())
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ url('/login') }}">Login</a>
+                        <a href="{{ url('/register') }}">Register</a>
+                    @endif
+                </div>
+
+
+
+                <ul>
+                                     <li>
+                                         <a href="{{ url('/logout') }}"
+                                             onclick="event.preventDefault();
+                                                      document.getElementById('logout-form').submit();">
+                                             Logout
+                                         </a>
+ 
+                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                             {{ csrf_field() }}
+                                         </form>
+                                     </li>
+                                 </ul>
+
 @endsection
+
